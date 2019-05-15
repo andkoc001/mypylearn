d736f836d881cbe0f82f6626e5e2edeca3a7bbd5
andkoc001 <and.koc001@gmail.com>
2019-05-15 23:30:40 +0100

    Sentence type changed from str to list.

--- 2019-05/sentence.py
+++ 2019-05/sentence.py
@@ -1,6 +1,11 @@
 # Andrzej Kocielski, 15-05-2019
-# Sentence
 # What character appears most in the sentence
-# ===
+# =====
 
 sentence = "This is a common interview task."
+letters = [sentence]
+
+counter = 0
+
+for letter in letters:
+    print(letter[:])
