f8bb0ac4d92f7e1114622d9b4d5c3fcc0c0f2f86
andkoc001 <and.koc001@gmail.com>
2019-05-15 23:45:33 +0100

    Solution adapterd from codewithmosh.com - dictionary used.

--- 2019-05/sentence.py
+++ 2019-05/sentence.py
@@ -1,11 +1,18 @@
 # Andrzej Kocielski, 15-05-2019
 # What character appears most in the sentence
+# Adapted from: https://codewithmosh.com/courses/417695/lectures/6781694
 # =====
 
 sentence = "This is a common interview task."
-letters = [sentence]
+# letters = [sentence]
 
-counter = 0
+char_frequency = {}  # empty dictionary
 
-for letter in letters:
-    print(letter[:])
+for char in sentence:  # iterates each char through the sentence
+    if char not in char_frequency:
+        # if the current char appears for the first time
+        char_frequency[char] = 1
+    else:
+        char_frequency[char] += 1  # if the current char has appeared before
+
+print(char_frequency)
