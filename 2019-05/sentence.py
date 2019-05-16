745951cd8446a2d3aca20a0b486521385c581e6d
andkoc001 <and.koc001@gmail.com>
2019-05-16 08:19:58 +0100

    Sorting and formating the output, as per codewithmosh.com.

--- 2019-05/sentence.py
+++ 2019-05/sentence.py
@@ -1,12 +1,12 @@
 # Andrzej Kocielski, 15-05-2019
 # What character appears most in the sentence
-# Adapted from: https://codewithmosh.com/courses/417695/lectures/6781694
 # =====
 
-sentence = "This is a common interview task."
+sentence = "This is a common interview question."
 # letters = [sentence]
 
-char_frequency = {}  # empty dictionary
+# Salution taken and adapted from: https://codewithmosh.com/courses/417695/lectures/6781694
+char_frequency = {}  # empty Dictionary
 
 for char in sentence:  # iterates each char through the sentence
     if char not in char_frequency:
@@ -15,4 +15,15 @@ for char in sentence:  # iterates each char through the sentence
     else:
         char_frequency[char] += 1  # if the current char has appeared before
 
-print(char_frequency)
+# print(char_frequency) # commented out, because of ugly formatting
+
+# sorting the result and formatting
+# because Dictionary is unsortable, we will convert it do Tuple
+# with the lambda function we will takes each item from the Dictionary and convert it to Tuple
+# sorted iterates through all items
+char_frequency_sorted = sorted(char_frequency.items(),
+                               # key parameter generates a new list; lambda takes key value (kv) of each item and assigns its value
+                               key=lambda kv: kv[1],
+                               reverse=True)
+
+print(char_frequency_sorted[0])
