31e054e783f41b45864c91f75007d89971e602d2
andkoc001 <and.koc001@gmail.com>
2019-07-10 22:36:05 +0100

    Just created, empty.

---
+++ prime_gen_spoj.py
@@ -0,0 +1,6 @@
+# Andrzej Kocielski, 2019-07-10
+# SPOJ.com solution to problem Prime generator, https://www.spoj.com/problems/PRIME1/.
+###
+
+# Import of required modules
+import math  # this module includes square root method
