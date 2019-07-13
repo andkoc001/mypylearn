2c10de5620550d34a12ae5aa16d280da5d8514bd
andkoc001 <and.koc001@gmail.com>
2019-07-13 09:00:01 +0100

    Working.

--- prime_gen_spoj.py
+++ prime_gen_spoj.py
@@ -4,15 +4,30 @@
 
 # Import of required modules
 import math  # this module includes square root method
+import time  # for duration of calculation
+
+m = 10
+n = 22
 
 # Number of test cases - entered by user, t<=10
-# t = int(input("Number of test cases: "))
+t = int(input("Number of test cases: "))
 
 # For each test case the user enters lower (m) and upper (n) limit for the test, 1<=m<=n<=1000000000, n-m<=100000
-# for i in range(t):
-# m = int(input("Lower limit: "))
-# n = int(input("Upper limit: "))
+for i in range(t):
 
-for j in range(2, 12+1):
-    if 12 % j != 0:
-        print(j)
+    m = int(input("Lower limit: "))
+    n = int(input("Upper limit: "))
+
+    # Calculation time
+    # t0 = time.time()
+
+    for num in range(m, n+1):
+        for j in range(2, 1+math.floor(math.sqrt(num))):
+            if num % j == 0:
+                break
+        else:
+            print(num)
+            pass
+
+    # t1 = time.time()
+    # print(t1-t0)
