cc76d14c9444d79bc5ac650206a1dedb103114bd
andkoc001 <and.koc001@gmail.com>
2019-07-13 15:12:29 +0100

    Works fine.

--- spoj-two-circles.py
+++ spoj-two-circles.py
@@ -7,48 +7,68 @@ import math  # this module includes square root method
 import time  # for duration of calculation
 
 
-def boundary_check(xo, yo, r):
-
-
-"""Checks if the boundary conditions are satisfied."""
-if (xo >= 0 and xo <= 10000) and (yo >= 0 and yo <= 10000) and (r >= 0 and r <= 10000):
-    return True
-
-
 def user_input():
-
-
-"""Asks user to input the required data."""
-  # Boundary contirions reminder
-  print("0 ≤ xo, yo,  ≤ 10000 and 0 < r1, r2 ≤ 10000 \n")
-   # Coords of centre and radious of 1st circle
-   print("Enter xo1, yo1 and r1 for the 1st circle respectively: ")
-    xo1 = int(input())
-    yo1 = int(input())
-    r1 = int(input())
+    """Asks user to input the required data."""
+    # Boundary contirions reminder
+    print("0 ≤ xo, yo  ≤ 10000 and 0 < r ≤ 10000 \n")
+    # Coords of centre and radious of 1st circle
+    print("Enter xo1, yo1 and r1 for the 1st circle respectively: ")
+    xo1 = float(input())
+    yo1 = float(input())
+    r1 = float(input())
     # Coords and radius of 2nd circle
     print("Enter xo2, yo2 and r2 for the 2nd circle respectively: ")
-    xo2 = int(input())
-    yo2 = int(input())
-    r2 = int(input())
+    xo2 = float(input())
+    yo2 = float(input())
+    r2 = float(input())
 
     # Boundary conditions check
-    if boundary_check:
-        print(xo1, yo1, r1, xo2, yo2, r2)
+    # c1 = boundary_check(xo1, yo1, r1)
+    # c2 = boundary_check(xo2, yo2, r2)
+    if (boundary_check(xo1, yo1, r1) and boundary_check(xo2, yo2, r2)) == True:
+        return xo1, yo1, r1, xo2, yo2, r2
     else:
-        print("Some entries out of range.")
+        return print("Some entered data is out of range. \n")
+
+
+def boundary_check(xo, yo, r):
+    """Checks if the boundary conditions are satisfied."""
+    if (xo >= 0 and xo <= 10000) and (yo >= 0 and yo <= 10000) and (r > 0 and r <= 10000):
+        return True
+
+
+def solution(xo1, yo1, r1, xo2, yo2, r2, i):
+    """Checks position of the circles."""
+
+    # Distance between centres of two circles
+    dist = math.sqrt((abs(xo1-xo2))**2 + (abs(yo1-yo2))**2)
+    print(dist)
+
+    print("*"*5, i+1, "solution:", "*"*5)
+    # Solution conditions
+    if ((dist+r1) == r2) or ((dist+r2) == r1):
+        print("E - internally tangible.")
+    elif ((dist+r1) < r2) or ((dist+r2) < r1):
+        print("I - one inside the other.")
+    else:
+        print("O - other scenario.")
 
 
 def main():
+    """Program checks relative position of the two circles."""
 
-
-"""Program checks relative position of the two circles."""
-
-  # Number of test cases - entered by user, t<=1000
-  cases = int(input("Number of test cases (max 1000 tests): "))
-   if 0 < cases < 1000:
+    # Number of test cases - entered by user, t<=1000
+    print("="*5)
+    cases = int(input("Number of test cases (max 1000 tests): "))
+    if 0 < cases < 1000:
         for i in range(cases):
-            user_input()
+            print("-"*5)
+            print(i+1, "pair of circles: ")
+            xo1, yo1, r1, xo2, yo2, r2 = user_input()
+            print("Data of the", i+1, "set of circles: ",
+                  xo1, yo1, r1, xo2, yo2, r2)
+            solution(xo1, yo1, r1, xo2, yo2, r2, i)
+            print("|"*22)
     else:
         print("Too many cases!")
 
