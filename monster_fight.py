143ad990a731ad2fc358fd3a24d71a65c5814350
andkoc001 <and.koc001@gmail.com>
2020-08-02 21:52:11 +0200

    Just started

---
+++ monster_fight.py
@@ -0,0 +1,11 @@
+# Monster Fight Game
+# Based on tutorial: https://www.youtube.com/watch?v=kDdTgxv2Vv0
+# Author: Andrzej Kocielski
+# Created: 02-08-2020
+# ------------------------------
+
+
+# -----
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
